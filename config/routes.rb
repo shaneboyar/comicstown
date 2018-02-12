5d1b6ea48c43dfb3580c9b08b75182cb39362671
Chris Tung <chptung@gmail.com>
2018-02-11 19:23:38 -0800

    added footer, /about, /terms, and /privac pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get "/about" => "static_pages#about"
+
+  get "/privacy" => "static_pages#privacy"
+
+  get "/terms" => "static_pages#terms"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'issues#index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
