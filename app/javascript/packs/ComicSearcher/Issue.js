dc976bc857da325e22df9bc29c8f5c61243b5873
Shane Boyar <shane.boyar@gmail.com>
2018-02-11 20:49:37 -0500

    Turbolinks things

--- app/javascript/packs/ComicSearcher/Issue.js
+++ app/javascript/packs/ComicSearcher/Issue.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
 const Issue = ({issue}) => (
-  <div className="IssueIndex_SearchResult">
-    <img src={issue.external_image_url} />
-    <h4>{issue.title}</h4>
-  </div>
+  <a href={`/issues/${issue.id}`}>
+    <div className="IssueIndex_SearchResult">
+      <img src={issue.external_image_url} />
+      <h4>{issue.title}</h4>
+    </div>
+  </a>
 );
 
 export default Issue;
