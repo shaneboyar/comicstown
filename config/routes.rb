790c09603a88ed8c2ef5fe8b894596ee176a4064
Chris Tung <chptung@gmail.com>
2018-02-12 13:33:56 -0800

    shane edits and also better series pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get "/about" => "static_pages#about"
-
-  get "/privacy" => "static_pages#privacy"
-
-  get "/terms" => "static_pages#terms"
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'issues#index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :publishers, :series, :issues, :writers, :artists
+
+  get "/about" => "static_pages#about"
+  get "/privacy" => "static_pages#privacy"
+  get "/terms" => "static_pages#terms"
+
 end
